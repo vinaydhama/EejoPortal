144de303be2f08fea8bf6b568ff1bf873cf9d582
vinaydhama <34028790+vinaydhama@users.noreply.github.com>
2025-09-07 20:49:36 +0530

    Modified File Names to propercase

--- www.eejo-portal.com/js/Pop.js
+++ www.eejo-portal.com/js/Pop.js
@@ -1,48 +1,48 @@
-function showPopup(message) {
-    document.getElementById("popupMessage").textContent = message;
-    document.getElementById("customPopup").style.display = "flex";
-  }
-
-  function onOk() {
-    // alert("You clicked OK");
-    document.getElementById("MessagePop").style.display = "none";
-    document.getElementById("overlaypop").style.display = "none";
-
-
-  }
-
-  function onCancel() {
-    // alert("You clicked Cancel");    
-    document.getElementById("MessagePop").style.display = "none";
-    document.getElementById("overlaypop").style.display = "none";
-
-  }
+// function showPopup(message) {
+//     document.getElementById("popupMessage").textContent = message;
+//     document.getElementById("customPopup").style.display = "flex";
+//   }
+  
+//   function onOk() {
+//     // alert("You clicked OK");
+//     document.getElementById("MessagePop").style.display = "none";
+//     document.getElementById("overlaypop").style.display = "none";
 
 
-  function showMessagePop(Show,Message="")
-  {
-    showhideDiv(false,"ActivityPop");
-    const MessagePop = document.getElementById("MessagePop");
-    if (MessagePop)
-      {      
-        if (Show)
-          {
-            MessagePop.style.display='block'
-            MessagePop.style.display = "flex";
-          }
-          else
-          {
-            MessagePop.style.display='none'
-          }
-          const popupMessage =  document.getElementById("popupMessage");
-          if (popupMessage)
-            {
-              popupMessage.textContent = Message;
-            }
-      }
+//   }
+  
+//   function onCancel() {
+//     // alert("You clicked Cancel");    
+//     document.getElementById("MessagePop").style.display = "none";
+//     document.getElementById("overlaypop").style.display = "none";
 
-  }
+//   }
+  
 
+//   function showMessagePop(Show,Message="")
+//   {
+//     showhideDiv(false,"ActivityPop");
+//     const MessagePop = document.getElementById("MessagePop");
+//     if (MessagePop)
+//       {      
+//         if (Show)
+//           {
+//             MessagePop.style.display='block'
+//             MessagePop.style.display = "flex";
+//           }
+//           else
+//           {
+//             MessagePop.style.display='none'
+//           }
+//           const popupMessage =  document.getElementById("popupMessage");
+//           if (popupMessage)
+//             {
+//               popupMessage.textContent = Message;
+//             }
+//       }
+  
+//   }
+  
 
 function showPopup(overlaypop) {
   document.getElementById(overlaypop).classList.add('show');
@@ -50,27 +50,32 @@ function showPopup(overlaypop) {
 
 function hidePopup(overlaypop) {
   document.getElementById(overlaypop).classList.remove('show');
-  showhideDiv(false, overlaypop, "");
+  showhideDiv(false,overlaypop,"");
 }
 
-function showhideDiv(state, id, BusyMsg = "") {
-  try {
-    if (BusyMsg != "") {
-      var BusyMsgobj = document.getElementById("BusyPopMsg");
-      BusyMsgobj.innerHTML = BusyMsg
-    }
-
-    var e = document.getElementById(id);
-    if (e) {
-      if (state == true) {
-        e.style.display = 'block'
-      }
-      else {
-        e.style.display = 'none'
-      }
-    }
-    // e.style.display = (e.style.display == 'block') ? 'none' : 'block';
-  } catch (error) {
-
+  function showhideDiv(state, id, BusyMsg = "") {
+    try {
+  if (BusyMsg!="")
+  
+  {
+  var BusyMsgobj = document.getElementById("BusyPopMsg");
+  BusyMsgobj.innerHTML = BusyMsg
   }
-}
+  
+  var e = document.getElementById(id);
+  if (e)
+    {
+  if (state==true)
+  {
+    e.style.display ='block'
+  }
+  else
+  {
+    e.style.display ='none'
+  }
+    }
+  // e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+  } catch (error) {
+        
+  }
+  }
